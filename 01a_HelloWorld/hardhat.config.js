8670dcd1117b43a333f11740c5e5b67014fdeb1c
RS <rspadinger@gmail.com>
2023-09-22 20:54:29 +0200

    add network to config file

--- 01a_HelloWorld/hardhat.config.js
+++ 01a_HelloWorld/hardhat.config.js
@@ -6,6 +6,19 @@ const { ALCHEMY_API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env
 module.exports = {
     solidity: {
         version: "0.8.20",
+        settings: {
+            optimizer: { enabled: true, runs: 200 },
+        },
     },
-    defaultNetwork: "localhost",      
+    defaultNetwork: "localhost",
+    networks: {
+        sepolia: {
+            url: ALCHEMY_API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
+    },
+    //npx hardhat verify --network sepolia ADDR "constructor argument"
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 }
